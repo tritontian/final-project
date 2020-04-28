928019f2f1cf6be5dbbd3c688044d3dd5a49ec5e
Tian GuoXin <tritontian@gmail.com>
2020-04-27 22:18:14 -0400

    v1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from "./home";
+import {Route, Switch} from "react-router";
+import {BrowserRouter} from "react-router-dom";
+import Login from "./Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+
+
+    render () {
+        // virtual root element will not be rendered to DOM tree.
+        return (
+            <React.Fragment>
+                <BrowserRouter>
+                    <Switch>
+                        <Route  path="/" component={Home}/>
+                        <Route  path="/Login" component={Login}/>
+                    </Switch>
+                </BrowserRouter>
+            </React.Fragment>
+        );
+    }
 }
-
-export default App;
